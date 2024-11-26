b8d321a310b8bd95795303b2515869de2608e103
KimSe-hyuk <rlatpgur627@gmail.com>
2024-11-26 16:17:29 +0900

    front

---
+++ select-front/src/main/java/com/example/tobi/selectfront/controller/Member/LoginController.java
@@ -0,0 +1,11 @@
+package com.example.tobi.selectfront.controller.Member;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController{
+
+    public String Login(){
+        return "login";
+    }
+}
